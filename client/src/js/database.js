8118a9b3ef6f201cfd3f3c2d398558fdb4b32fad
N-lara <n_lara01@yahoo.com>
2024-01-17 21:36:33 -0600

    added link to deployed site

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,9 @@ const initdb = async () =>
     },
   });
 
-// /////////////: Add logic to a method that accepts some content and adds it to the database
+// ///////////: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -22,7 +23,7 @@ export const putDb = async (content) => {
   const result = await request;
   console.log('Data saved to the database', result);
 }
-// /////// Add logic for a method that gets all the content from the database
+// ///////////: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,14 +6,10 @@
 // THEN I find that the content in the text editor has been saved with IndexedDB
 // WHEN I reopen the text editor after closing it
 // THEN I find that the content in the text editor has been retrieved from our IndexedDB
-// WHEN I click on the Install button
-// THEN I download my web application as an icon on my desktop
 // WHEN I load my web application
 // THEN I should have a registered service worker using workbox
 // WHEN I register a service worker
 // THEN I should have my static assets pre cached upon loading along with subsequent pages and static assets
-// WHEN I deploy to Render
-// THEN I should have proper build scripts for a webpack application
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');//use
 const WebpackPwaManifest = require('webpack-pwa-manifest');
